5d0367a44ecee97044eb69ffe6f32d7cef9bb8d5
hpz <1090377416@qq.com>
2020-05-03 18:10:19 +0800

    完成活动本地开发部分

--- miniprogram/pages/activity/activity.js
+++ miniprogram/pages/activity/activity.js
@@ -1,66 +1,82 @@
 // pages/activity/activity.js
+const db = wx.cloud.database()
+const _ = db.command
+const activityDB = db.collection('Activity')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    activity: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.reqActivity();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+  /**
+   * 请求Activity数据库,获取数据
+   */
+  reqActivity: function() {
+    activityDB.orderBy('activityNumber', 'desc')
+      .get()
+      .then(res => {
+        console.log(res.data)
+        this.setData({
+          activity: res.data
+        })
+      })
   }
 })
